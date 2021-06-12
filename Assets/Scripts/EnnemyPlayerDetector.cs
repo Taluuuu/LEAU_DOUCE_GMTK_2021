cbf4c6320faa767dbd5ecfdee59d617c509725d3
3FI <ec10pc8@gmail.com>
2021-06-11 21:58:35 -0400

    TileMap Done

--- Assets/Scripts/EnnemyPlayerDetector.cs
+++ Assets/Scripts/EnnemyPlayerDetector.cs
@@ -4,13 +4,25 @@ using UnityEngine;
 
 public class EnnemyPlayerDetector : MonoBehaviour
 {
-    public Player _player;
-
+    private Rigidbody rb;
+    public Player Player1;
+    public Player Player2;
+    private void Awake()
+    {
+        rb = transform.GetComponent­<Rigidbody>();
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            _player.Hit();
+            if ((Player1.transform.position-rb.position).magnitude > (Player2.transform.position - rb.position).magnitude)
+            {
+                Player2.Hit();
+            }
+            if ((Player2.transform.position - rb.position).magnitude > (Player1.transform.position - rb.position).magnitude)
+            {
+                Player1.Hit();
+            }
         }
     }
 }
