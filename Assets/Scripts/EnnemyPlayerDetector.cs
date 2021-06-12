930b699a883a7058c8ecb865886968122bf33ea9
Ram0u <remi.cote@hotmail.ca>
2021-06-11 22:35:31 -0400

    Etienne pas bo

--- Assets/Scripts/EnnemyPlayerDetector.cs
+++ Assets/Scripts/EnnemyPlayerDetector.cs
@@ -4,25 +4,16 @@ using UnityEngine;
 
 public class EnnemyPlayerDetector : MonoBehaviour
 {
-    private Rigidbody rb;
-    public Player Player1;
-    public Player Player2;
+
     private void Awake()
     {
-        rb = transform.GetComponent­<Rigidbody>();
+
     }
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            if ((Player1.transform.position-rb.position).magnitude > (Player2.transform.position - rb.position).magnitude)
-            {
-                Player2.Hit();
-            }
-            if ((Player2.transform.position - rb.position).magnitude > (Player1.transform.position - rb.position).magnitude)
-            {
-                Player1.Hit();
-            }
+            other.GetComponent<Player>().Hit();
         }
     }
 }
