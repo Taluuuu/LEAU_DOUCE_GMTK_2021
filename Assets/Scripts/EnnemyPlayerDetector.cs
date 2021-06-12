00d2603ecb2ab590575bb131cadef54724d06658
3FI <ec10pc8@gmail.com>
2021-06-11 22:09:00 -0400

    Update EnnemyPlayerDetector.cs

--- Assets/Scripts/EnnemyPlayerDetector.cs
+++ Assets/Scripts/EnnemyPlayerDetector.cs
@@ -5,23 +5,23 @@ using UnityEngine;
 public class EnnemyPlayerDetector : MonoBehaviour
 {
     private Rigidbody rb;
-    public Player Player1;
-    public Player Player2;
+    private Player[] Players;
     private void Awake()
     {
         rb = transform.GetComponent­<Rigidbody>();
+        Players = Player.FindObjectsOfType<Player>();
     }
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            if ((Player1.transform.position-rb.position).magnitude > (Player2.transform.position - rb.position).magnitude)
+            if ((Players[0].transform.position-rb.position).magnitude > (Players[1].transform.position - rb.position).magnitude)
             {
-                Player2.Hit();
+                Players[1].Hit();
             }
-            if ((Player2.transform.position - rb.position).magnitude > (Player1.transform.position - rb.position).magnitude)
+            if ((Players[1].transform.position - rb.position).magnitude > (Players[0].transform.position - rb.position).magnitude)
             {
-                Player1.Hit();
+                Players[0].Hit();
             }
         }
     }
